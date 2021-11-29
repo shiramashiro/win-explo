36fc37d394c5d34add83c2dd48b9fe270eb2eb13
shiramashiro <zhengrenfu@outlook.com>
2021-11-29 22:34:00 +0800

    修改变量名，添加注释

--- classify_files.py
+++ classify_files.py
@@ -1,23 +1,30 @@
 import os
 import shutil
 
+'''
+这是一个自动化分类文件的模块
+'''
 
-def classify(base_dir):
-    files = os.listdir(base_dir)
+
+'''
+对目录中的文件进行分类，并且可以按照时间进行再分类。
+'''
+def classify(basedir, timelen):
+    files = os.listdir(basedir)
     # 遍历 base_dir 目录下的所有文件以及目录
-    for file_item in files:
+    for file in files:
         # 当前遍历的数组元素，以 . 分割，得到一个文件名以及后缀名的数组
-        filetext = os.path.splitext(file_item)
+        filename = os.path.splitext(file)
         # 判断目录下的当前对象是否为文件，文件则返回 True，目录则返回 False
-        source_obj = os.path.join(base_dir, file_item)
-        if os.path.isfile(source_obj):
+        source = os.path.join(basedir, file)
+        if os.path.isfile(source):
             # 该文件类型的目录
-            dest_dir = os.path.join(base_dir, filetext[1].split('.')[1].upper())
+            destdir = os.path.join(basedir, filename[1].split('.')[1].upper())
             # 如果存放该文件的目录不存在，则创建
-            if not os.path.exists(dest_dir):
-                os.mkdir(dest_dir)
+            if not os.path.exists(destdir):
+                os.mkdir(destdir)
             # 将该文件存放到符合的目录中
-            shutil.move(source_obj, dest_dir)
+            shutil.move(source, destdir)
 
 
-classify(r'E:\CommonFolders\Pictures')
+classify(r'E:\CommonFolders\Downloads')
