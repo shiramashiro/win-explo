0bf222c16900230b4850ad9be81590169ceba801
shiramashiro <zhengrenfu@outlook.com>
2021-12-04 01:40:01 +0800

    分割函数的功能

--- classify_files.py
+++ classify_files.py
@@ -3,73 +3,76 @@ import shutil
 import time
 
 
-def dir_exists(dir):
-    if not os.path.exists(dir):
-        os.makedirs(dir)
-    return dir
-
-
-def time_sort(obj_path, time_order, dest_dir):
+def pathexists(filepath):
     """
-    按照时间进行排序，可以按照年、月、日进行分类。月是年-月；日是年-月-日的形式进行分类。
-    :param obj_path: 文件的绝对路径
-    :param time_order: 是否以时间形式进行分类
-    :param dest_dir: 移动到目标目录
+    判断文件或文件夹是否存在
+    :param filepath: 传入一个文件或文件夹的绝对路径
+    :return: 无论是否存在，都会返回一个绝对路径
     """
-    # 获取文件最后修改的时间戳
-    timestamp = os.path.getmtime(obj_path)
-    # 格式化时间，并以-分割开来
-    timesarr = time.strftime("%Y-%m-%d", time.localtime(timestamp)).split('-')
-    # 以时间形式创造的目录
-    build_dir = None
-    if time_order == 'y':
-        build_dir = dir_exists(os.path.join(dest_dir, timesarr[0]))
-    elif time_order == 'm':
-        build_dir = dir_exists(os.path.join(dest_dir, timesarr[0], timesarr[1]))
-    elif time_order == 'd':
-        build_dir = dir_exists(os.path.join(dest_dir, timesarr[0], timesarr[1], timesarr[2]))
-
-    if build_dir is not None:
-        shutil.move(obj_path, build_dir)
+    if not os.path.exists(filepath):
+        os.makedirs(filepath)
+    return filepath
 
 
-def sort(base_dir, sub_dir_name=None, time_order=None):
+def typesort(filepath, filetype, basepath):
     """
-    对当前目录进行文件分类。
-        你可以选择给每一个对应的类型目录下再以时间进行分类存放。
-        你可以选择在当前目录下创建一个子目录，然后再进行分类。
-    :param base_dir: 当前目录，即你要操作的目录的绝对路径
-    :param sub_dir_name: 是否在当前目录下创建一个子目录
-    :param time_order: 是否以时间形式进行分类
+    按照文件类型进行分类
+    :param filepath: 文件的绝对路径
+    :param filetype: 文件类型，比如 .zip .rar .png .jpg ...
+    :param basepath: 分类之后保存的目标路径,之所以是base命名，是因为在后续操作中会凭借路径
     """
-    # 列出目录下的所有文件以及文件夹
-    objs = os.listdir(base_dir)
-    # 对这些文件以及文件夹进行循环
-    for obj in objs:
-        # 获得该文件或者文件夹的绝对路径
-        obj_path = os.path.join(base_dir, obj)
-        # 根据绝对路径判断该对象是否为一个文件
-        if os.path.isfile(obj_path):
-            # 将文件名以及文件后缀名分割开来，例如 test.py => ['test', '.py']
-            filename_suffix = os.path.splitext(obj)
-            # 希望将当前目录下所有的文件存放到指定的子目录下
-            child_dir = None
-            if sub_dir_name is not None:
-                child_dir = dir_exists(os.path.join(base_dir, sub_dir_name))
-            # 有子目录或没有子目录的情况
-            dest_dir = None
-            if child_dir is not None:
-                dest_dir = dir_exists(os.path.join(child_dir, filename_suffix[1].split('.')[1].upper()))
-            else:
-                dest_dir = dir_exists(os.path.join(base_dir, filename_suffix[1].split('.')[1].upper()))
+    filepatt = os.path.splitext(filepath)[1].split('.')[1]
+    if filetype == filepatt:
+        destdir = pathexists(os.path.join(basepath, filepatt.upper()))
+        shutil.move(filepath, destdir)
 
-            # 是否以时间进行分类
-            if time_order is not None:
-                # 以时间进行分类，然后再移动到对应类型的目录
-                time_sort(obj_path, time_order, dest_dir)
-            else:
-                # 不以时间进行分类，直接移动到对应类型的目录
-                shutil.move(obj_path, dest_dir)
+
+def timesort(filepath, timemode, basepath):
+    """
+    按照时间进行分类。
+    :param filepath: 文件路径
+    :param timemode: 时间模式，年、月、日三种模式
+    :param basepath: 分类之后保存的目标路径，之所以是base命名，是因为在后续操作中会凭借路径
+    """
+    timestamp = os.path.getmtime(filepath)
+    timenodes = time.strftime("%Y-%m-%d", time.localtime(timestamp)).split('-')
+    if timemode == 'y':
+        destdir = pathexists(os.path.join(basepath, timenodes[0]))
+    elif timemode == 'm':
+        destdir = pathexists(os.path.join(basepath, timenodes[0], timenodes[1]))
+    else:
+        destdir = pathexists(os.path.join(basepath, timenodes[0], timenodes[1], timenodes[2]))
+    shutil.move(filepath, destdir)
+
+
+def mtsort(filepath, basepath, timemode, filetype):
+    print('先按时间进行分类，然后再将该类型的文件进行分类')
+
+def tmsort(filepath, basepath, filetype, timemode):
+    print('先将该类型的文件进行分类，然后再按时间分类')
+
+
+def sortfiles(usefuldir, destdir, timemode=None, filetype=None, order=None):
+    """
+    对指定目录下的文件进行分类
+    :param usefuldir: 可实际操作的目录
+    :param destdir: 目标目录
+    :param timemode: 时间模式
+    :param filetype: 文件类型
+    :param order: 时间和文件类型排序顺序，mt：时间在前，类型在后；tm：类型在前，时间在后
+    """
+    files = os.listdir(usefuldir)
+    for file in files:
+        filepath = os.path.join(usefuldir, file)
+        if os.path.isfile(filepath):
+            print(filepath)
+            if timemode is not None:
+                timesort(filepath, timemode, destdir)
+            elif filetype is not None:
+                typesort(filepath, filetype, destdir)
+
+
+sortfiles(usefuldir=r'E:\CommonFolders\Downloads', destdir=r'E:\CommonFolders\Downloads', filetype='zip')
 
 
 def unsort(base_dir):
@@ -81,7 +84,3 @@ def unsort(base_dir):
     objs = os.listdir(base_dir)
     for obj in objs:
         print(os.listdir(os.path.join(base_dir, obj)))
-
-
-# unsort(r'E:\CommonFolders\Downloads')
-sort(r'E:\CommonFolders\Downloads', time_order='m')
